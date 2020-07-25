ec9875d792326eb709d14169503c87541e7ab877
Thiago R Costa <thiago_8777@hotmail.com>
2020-07-24 21:05:47 -0300

    Config Iniciais

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: ['./*.html'],
+  theme: {
+    colors: {
+      red: '#ea5353',
+      cyan: '#45d3d3',
+      orange: '#fcaf4a',
+      blue: '#549ef2',
+      darkblue: '#4c4e61',
+      grayishblue: '#a3a5ae',
+      lightgray: '#fafafa'
+    },
+    
+    fontFamily: {
+      'body': 'Poppins'
+    },
+    fontWeight: {
+      light: '200', 
+      normal: '400', 
+      bold: '600'
+    },
+    container: {
+      center: true
+    },
+    extends: {}
+  },
+  variants: {},
+  plugins: [],
+}
